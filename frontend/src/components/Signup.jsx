872002b0e3d450036888f2026cfecc8d830f6ac6
Akshita Guru <akshitaguru16@gmail.com>
2024-08-25 18:32:43 +0530

    update

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,92 @@
+
+import axios from "axios";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import React, { useState } from "react";
+
+function Signup() {
+    const [input, setInput] = useState({
+        username:"",
+        email:"",
+        password:""
+    });
+    const changeEventHandler = (e) => {
+        setInput({...input, [e.target.name]:e.target.value});
+    }
+
+    
+    const signupHandler = async (e) => {
+        e.preventDefault();
+        console.log(input);
+        try {
+          const res = await axios.post('http://localhost:8000/api/v1/user/register',input, {
+           
+            headers:{
+                'Content-Type':'application/json'
+            },
+            withCredentials:true
+          });
+          if(res.data.success) {
+            toast.success(res.data.message);
+          }
+ 
+        } 
+        catch (error) {
+            console.log(error);
+           
+        }
+    }
+
+
+  return (
+    <div className="flex items-center w-screen h-screen justify-center">
+      <form onSubmit={signupHandler} className="shadow-lg flex flex-col gap-5 p-8">
+        <div className='my-4'>
+          <h1 className="text-center font-bold text-xl">LOGO</h1>
+          <p className="text-sm text-center">Signup to see photos and videos from your friends</p>
+        </div>
+        <div>
+            <span className=" font-medium ">
+                Username
+            </span>
+            <Input 
+            type="text" 
+            name = "username" 
+            value={input.username} 
+            onChange={changeEventHandler}
+            className = "focus-visible:ring-transparent my-2"/>
+        </div>
+
+        <div>
+            <span className=" font-medium ">
+                Email
+            </span>
+            <Input 
+            type="email" 
+            name = "email" 
+            value={input.email} 
+            onChange={changeEventHandler}
+            className = "focus-visible:ring-transparent my-2"/>
+        </div>
+
+        <div>
+            <span className=" font-medium ">
+                Password
+            </span>
+            <Input 
+            type="password" 
+            name = "password" 
+            value={input.password} 
+            onChange={changeEventHandler}
+            className = "focus-visible:ring-transparent my-2"/>
+        </div>
+
+           <Button type="submit">
+            Signup
+           </Button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
