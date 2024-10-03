72c167ecce36c79c5fad49c471eff9f4167cf4bd
Akshita Guru <akshitaguru16@gmail.com>
2024-10-04 00:08:14 +0530

    updated

--- frontend/src/components/Leftsidebar.jsx
+++ frontend/src/components/Leftsidebar.jsx
@@ -1,29 +1,74 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
-import { Home } from 'lucide-react'
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  Heart,
+  Home,
+  LogOut,
+  MessageCircle,
+  PlusSquare,
+  Search,
+  TrendingUp,
+} from "lucide-react";
+import React from "react";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 const sidebarItems = [
-    {icon:<Home/>,text:"Home"},
-    {icon:<Search/>,text:"Search"},
-    {icon:<TredingUp/>,text:"Explore"},
-    {icon:<MessageCircle/>,text:"Home"},
-    {icon:<Heart/>,text:"Notifications"},
-    {icon:<PlusSquare/>,text:"Create"},
-    {icon: (
-      <Avatar>
-  <AvatarImage src="https://github.com/shadcn.png" />
-  <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-    ), text:"Profile"},
-    {icon:<LogOut/>,text:"Logout"},
-]
+  { icon: <Home />, text: "Home" },
+  { icon: <Search />, text: "Search" },
+  { icon: <TrendingUp />, text: "Explore" },
+  { icon: <MessageCircle />, text: "Messages" },
+  { icon: <Heart />, text: "Notifications" },
+  { icon: <PlusSquare />, text: "Create" },
+  {
+    icon: (
+      <Avatar className="w-6 h-6">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    ),
+    text: "Profile",
+  },
+  { icon: <LogOut />, text: "Logout" },
+];
 
-const leftsidebar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Leftsidebar = () => {
+  const navigate = useNavigate();
+
+
+const logouthandler = async() => {
+  try{
+        const res = await axios.get('http://localhost:8000/api/v1/user/logout', {withCrendentials:true});
+        if(res.data.success)
+        {
+          navigate("/login");
+          toast.success(res.data.message);
+        }
+  }
+  catch (error)  {
+    toast.error(error.response.data.message);
+  }
 }
 
-export default leftsidebar
+  const sidebarHandler = (textType) => {
+    if(textType === 'Logout')logouthandler();
+  }
+  return (
+    <div className="fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen">
+      <div className="flex flex-col">
+        <h1>LOGO</h1>
+        <div>
+          {sidebarItems.map((item, index) => {
+            return (
+              <div onClick={() => sidebarHandler(item.text)} key={index} className="flex items-center gap-3 relative hover:bg-gray-100 cursor-pointer rounded-lg p-3 my-3">
+                {item.icon}
+                <span>{item.text}</span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Leftsidebar;
