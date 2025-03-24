0d5f0086accb6ec9b55f89963a42a2cb1bb1efb1
Akshita Guru <akshitaguru16@gmail.com>
2025-03-25 02:10:07 +0530

    updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,23 +1,110 @@
 import useGetUserProfile from "@/hooks/useGetUserProfile";
 import store from "@/redux/store";
-import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { AtSign } from "lucide-react";
 
 const Profile = () => {
   const params = useParams();
   const userId = params.id;
   useGetUserProfile(userId);
 
-
-  const {userProfile} =  useSelector(store=>store.auth);
+  const { userProfile } = useSelector((store) => store.auth);
+  const isLoggedInUserProfle = true;
+  const isFollowing = false;
   console.log(userProfile);
   return (
-    <div>
-      <Avatar>
-        <AvatarImage />
-      </Avatar>
+    <div className="flex max-w-5xl justify-center mx-auto pl-10">
+      <div className="flex flex-col gap-20 p-8">
+        <div className="grid grid-cols-2">
+          <section className="flex items-center justify-center">
+            <Avatar className="h-32 w-32">
+              <AvatarImage
+                src={userProfile?.profilePicture}
+                alt="profilephoto"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </section>
+          <section>
+            <div className="flex flex-col gap-5">
+              <div className="flex items-center gap-2">
+                <span>{userProfile?.username}</span>
+                {isLoggedInUserProfle ? (
+                  <>
+                    <Button
+                      variant="secondary"
+                      className="hover:bg-gray-200 h-8"
+                    >
+                      Edit profile
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      className="hover:bg-gray-200 h-8"
+                    >
+                      View archive
+                    </Button>
+
+                    <Button
+                      variant="secondary"
+                      className="hover:bg-gray-200 h-8"
+                    >
+                      Ad tools
+                    </Button>
+                  </>
+                ) : isFollowing ? (
+                  <>
+                    <Button variant="secondary" className="h-8">
+                      Unfollow
+                    </Button>
+                    <Button variant="secondary" className="h-8">
+                      Message
+                    </Button>
+                  </>
+                ) : (
+                  <Button className="bg-[#0095F6] hover:bg-[#3192d2] h-8">
+                    Follow
+                  </Button>
+                )}
+              </div>
+              <div className="flex items-center gap-4">
+                <p>
+                  <span className="font-semibold">
+                    {" "}
+                    {userProfile?.posts.length}
+                  </span>
+                  posts{" "}
+                </p>
+                <p>
+                  <span className="font-semibold">
+                    {" "}
+                    {userProfile?.followers.length}{" "}
+                  </span>
+                  followers
+                </p>
+                <p>
+                  <span className="font-semibold">
+                    {userProfile?.following.length}
+                  </span>
+                  following{" "}
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="font-semibold">
+                  {userProfile?.bio || 'bio here...'}
+                </span>
+                <Badge className="w-fit" variant='secondary'><AtSign/> <span className="pl-1">{userProfile?.username}</span></Badge>
+                <span>🤯Code with Coffee!!</span>
+                <span>DM for collaboration</span>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
