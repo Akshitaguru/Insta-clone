07e9a8491c285332ced75a24f9b1697047ff291b
Akshita Guru <akshitaguru16@gmail.com>
2025-03-25 01:01:11 +0530

    updated profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import useGetUserProfile from "@/hooks/useGetUserProfile";
+import store from "@/redux/store";
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
+  const params = useParams();
+  const userId = params.id;
+  useGetUserProfile(userId);
+
+
+  const {userProfile} =  useSelector(store=>store.auth);
+  console.log(userProfile);
   return (
     <div>
-       Profile Page
+      <Avatar>
+        <AvatarImage />
+      </Avatar>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
