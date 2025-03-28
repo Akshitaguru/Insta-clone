03c64374d29dbb7d2a5bc0341e7df434375dfd0c
Akshita Guru <akshitaguru16@gmail.com>
2025-03-29 01:38:31 +0530

    updated

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 
 const EditProfile = () => {
   const { user } = useSelector((store) => store.auth);
   return (
     <div className="flex max-w-2xl mx-auto pl-10">
-      <section>
+      <section className="flex flex-col gap-6 w-full">
         <h1 className="font-bold text-xl">Edit Profile</h1>
-        <div className="flex items-center gap-2">
-          <Avatar>
-            <AvatarImage src={user?.profilepicture} alt="post_image" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+        <div className="flex items-center justify-between bg-gray-100 rounded-xl p-4">
+          <div className="flex items-center gap-3">
+            <Avatar>
+              <AvatarImage src={user?.profilepicture} alt="post_image" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
 
-          <div>
-            <h1 className="font-bold text-sm">{user?.username}</h1>
-            <span className="text-gray-600 ">
-              {user?.bio || "Bio here..."}
-            </span>
+            <div>
+              <h1 className="font-bold text-sm">{user?.username}</h1>
+              <span className="text-gray-600 ">
+                {user?.bio || "Bio here..."}
+              </span>
+            </div>
           </div>
+          <input type="file" className="hidden"/>
+          <Button className="bg-[#0095F6] h-8 hover:bg-[#318bc7]">Change Photo</Button>
         </div>
       </section>
     </div>
