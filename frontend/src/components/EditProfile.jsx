cd04d8ce65936783f11d065bc75efb9e7dcb887f
Akshita Guru <akshitaguru16@gmail.com>
2025-03-30 00:28:28 +0530

    updated

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -3,13 +3,37 @@ import { useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { useRef } from "react";
+import { Textarea } from "./ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { useState } from "react";
+import axios from "axios";
+import { Loader, Loader2 } from "lucide-react";
 
 const EditProfile = () => {
   const imageRef = useRef();
+  const [loading, setLoading] = useState(false);
   const { user } = useSelector((store) => store.auth);
+
+  const editProfileHandler = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex max-w-2xl mx-auto pl-10">
-      <section className="flex flex-col gap-6 w-full">
+      <section className="flex flex-col gap-6 w-full my-8">
         <h1 className="font-bold text-xl">Edit Profile</h1>
         <div className="flex items-center justify-between bg-gray-100 rounded-xl p-4">
           <div className="flex items-center gap-3">
@@ -25,8 +49,43 @@ const EditProfile = () => {
               </span>
             </div>
           </div>
-          <input ref={imageRef} type="file" className="hidden"/>
-          <Button onClick={ () => imageRef?.current.click()} className="bg-[#0095F6] h-8 hover:bg-[#318bc7]">Change Photo</Button>
+          <input ref={imageRef} type="file" className="hidden" />
+          <Button
+            onClick={() => imageRef?.current.click()}
+            className="bg-[#0095F6] h-8 hover:bg-[#318bc7]"
+          >
+            Change Photo
+          </Button>
+        </div>
+        <div>
+          <h1 className="font-bold text-xl mb-2">Bio</h1>
+          <Textarea name="bio" className="focus-visible:ring-transparent" />
+        </div>
+        <div>
+          <h1 className="font-bold mb-2">Gender</h1>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="apple">Male</SelectItem>
+                <SelectItem value="banana">Female</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex justify-end">
+          {loading ? (
+            <Button className="w-fit bg-[#0095F6] hover:bg-[#2a8ccd]">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : (
+            <Button className="w-fit bg-[#0095F6] hover:bg-[#2a8ccd]">
+              Submit
+            </Button>
+          )}
         </div>
       </section>
     </div>
