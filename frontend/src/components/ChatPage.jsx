d35967eda0fff632fd93a69706b0a82de2db6353
Akshita Guru <akshitaguru16@gmail.com>
2025-03-31 22:33:13 +0530

    updated

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { setSelectedUser } from "@/redux/authSlice";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { MessageCircle, MessageCircleCode } from "lucide-react";
 
 const ChatPage = () => {
-  const { user, suggestedUsers, selectedUser } = useSelector((store) => store.auth);
+  const { user, suggestedUsers, selectedUser } = useSelector(
+    (store) => store.auth
+  );
   const isOnline = true;
   const dispatch = useDispatch();
   return (
     <div className="flex ml-[16%] h-screen">
-      <section>
+      <section className="w-full md:w-1/4 my-8">
         <h1 className="font-bold mb-4 px-3 text-xl">{user?.username}</h1>
         <hr className="mb-4 border-gray-400" />
         <div className="overflow-y-auto h-[80vh] ">
           {suggestedUsers.map((suggestedUser) => {
             return (
-              <div onClick={() => dispatch(setSelectedUser(suggestedUser))} className="flex gap-3 items-center p-3 hover:bg-gray-50 cursor-pointer">
-                <Avatar>
+              <div
+                onClick={() => dispatch(setSelectedUser(suggestedUser))}
+                className="flex gap-3 items-center p-3 hover:bg-gray-50 cursor-pointer"
+              >
+                <Avatar className ='w-14 h-14'>
                   <AvatarImage src={suggestedUser.profilePicture} />
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
@@ -35,6 +43,39 @@ const ChatPage = () => {
           })}
         </div>
       </section>
+      {selectedUser ? (
+        <section className="flex-1 border-l border-l-gray-300 flex flex-col h-full">
+          <div className="flex gap-3 items-center px-3 py-2 border-b border-gray-300 sticky top-0 bg-white z-10">
+            <Avatar>
+              <AvatarImage src={selectedUser?.profilePicture} alt="profile" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <span>{selectedUser?.username}</span>
+            </div>
+          </div>
+          messages aayenge
+          messages aayenge
+          messages aayenge
+
+          messages aayenge
+
+          <div className="flex items-center p-4 border-t bordet-t-gray-300">
+            <Input
+              type="text"
+              className="flex-1 mr-2 focus-visible:ring-transparent"
+              placeholder="Messages..."
+            />
+            <Button>Send</Button>
+          </div>
+        </section>
+      ) : (
+        <div className="flex flex-col items-center justify-center mx-auto">
+          <MessageCircleCode className="w-32 h-32 my-4" />
+          <h1 className="font-medium text-xl">Your messages</h1>
+          <span>Send a message to start a chat.</span>
+        </div>
+      )}
     </div>
   );
 };
