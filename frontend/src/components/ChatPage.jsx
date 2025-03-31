a42e68a79c9926ecd7685661b92ecabd9baf81fe
Akshita Guru <akshitaguru16@gmail.com>
2025-03-31 22:08:41 +0530

    updated

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Avatar, AvatarImage } from "./ui/avatar";
+import { useDispatch, useSelector } from "react-redux";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { setSelectedUser } from "@/redux/authSlice";
 
 const ChatPage = () => {
-  const { user, suggestedUsers } = useSelector((store) => store.auth);
+  const { user, suggestedUsers, selectedUser } = useSelector((store) => store.auth);
+  const isOnline = true;
+  const dispatch = useDispatch();
   return (
     <div className="flex ml-[16%] h-screen">
       <section>
         <h1 className="font-bold mb-4 px-3 text-xl">{user?.username}</h1>
-        <hr className="mb-4 border-gray-400"/>
+        <hr className="mb-4 border-gray-400" />
         <div className="overflow-y-auto h-[80vh] ">
-{
-    suggestedUsers.map((suggestedUser) => {
-        return (
-            <div>
+          {suggestedUsers.map((suggestedUser) => {
+            return (
+              <div onClick={() => dispatch(setSelectedUser(suggestedUser))} className="flex gap-3 items-center p-3 hover:bg-gray-50 cursor-pointer">
                 <Avatar>
-                    <AvatarImage src={suggestedUser.profilePicture} />
-                    </Avatar>
+                  <AvatarImage src={suggestedUser.profilePicture} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <span className="font-medium">{suggestedUser?.username}</span>
+                  <span
+                    className={`text-xs font-bold ${
+                      isOnline ? "text-green-600" : "text-red-600"
+                    }`}
+                  >
+                    {isOnline ? "online" : "offline"}
+                  </span>
                 </div>
-        )
-    })
-}
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
