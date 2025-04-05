5bc4dc0d2481e6f75c8af01cd8be5b843585db4b
Akshita Guru <akshitaguru16@gmail.com>
2025-04-06 00:59:02 +0530

    notifcations

--- frontend/src/components/Leftsidebar.jsx
+++ frontend/src/components/Leftsidebar.jsx
@@ -1,4 +1,3 @@
-
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import axios from "axios";
 import {
@@ -13,25 +12,32 @@ import {
 import React from "react";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { setAuthUser } from "@/redux/authSlice";
 import { useState } from "react";
 import CreatePost from "./CreatePost";
 import { setPosts, setSelectedPost } from "@/redux/postSlice";
-
-
+import store from "@/redux/store";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
 
 const Leftsidebar = () => {
   const navigate = useNavigate();
- const { user } = useSelector(store=>store.auth);
- const dispatch = useDispatch();
- const [open, setOpen] = useState(false);
+  const { user } = useSelector((store) => store.auth);
+  const { likeNotification } = useSelector(
+    (store) => store.realTimeNotification
+  );
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
   const logouthandler = async () => {
     try {
-      const res = await axios.get('http://localhost:8000/api/v1/user/logout', { withCredentials: true });
-      console.log('Logout response:', res); // Add this line to log the response
-      if (res && res.data && res.data.success) { // Add checks for res and res.data
+      const res = await axios.get("http://localhost:8000/api/v1/user/logout", {
+        withCredentials: true,
+      });
+      console.log("Logout response:", res); // Add this line to log the response
+      if (res && res.data && res.data.success) {
+        // Add checks for res and res.data
         dispatch(setAuthUser(null));
         dispatch(setSelectedPost(null));
         dispatch(setPosts([]));
@@ -39,47 +45,46 @@ const Leftsidebar = () => {
 
         toast.success(res.data.message);
       } else {
-        console.error('Logout failed:', res); // Log an error if the response is not successful
+        console.error("Logout failed:", res); // Log an error if the response is not successful
       }
     } catch (error) {
-      console.error('Logout error:', error); // Log the error
+      console.error("Logout error:", error); // Log the error
       toast.error(error.response.data.message);
     }
-  }
+  };
 
- 
   const sidebarHandler = (textType) => {
-    if (textType === 'Logout') {
-        logouthandler();
+    if (textType === "Logout") {
+      logouthandler();
     } else if (textType === "Create") {
       setOpen(true);
     } else if (textType === "Profile") {
-        navigate(`/profile/${user?._id}`);
+      navigate(`/profile/${user?._id}`);
     } else if (textType === "Home") {
-        navigate("/");
-    } else if (textType === 'Messages') {
-        navigate("/chat");
+      navigate("/");
+    } else if (textType === "Messages") {
+      navigate("/chat");
     }
-}
+  };
 
-const sidebarItems = [
-  { icon: <Home />, text: "Home" },
-  { icon: <Search />, text: "Search" },
-  { icon: <TrendingUp />, text: "Explore" },
-  { icon: <MessageCircle />, text: "Messages" },
-  { icon: <Heart />, text: "Notifications" },
-  { icon: <PlusSquare />, text: "Create" },
-  {
-    icon: (
-      <Avatar className="w-6 h-6">
-        <AvatarImage src={user?.profilePicture}  alt="@shadcn"/>
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-    ),
-    text: "Profile",
-  },
-  { icon: <LogOut />, text: "Logout" },
-];
+  const sidebarItems = [
+    { icon: <Home />, text: "Home" },
+    { icon: <Search />, text: "Search" },
+    { icon: <TrendingUp />, text: "Explore" },
+    { icon: <MessageCircle />, text: "Messages" },
+    { icon: <Heart />, text: "Notifications" },
+    { icon: <PlusSquare />, text: "Create" },
+    {
+      icon: (
+        <Avatar className="w-6 h-6">
+          <AvatarImage src={user?.profilePicture} alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      ),
+      text: "Profile",
+    },
+    { icon: <LogOut />, text: "Logout" },
+  ];
 
   return (
     <div className="fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen">
@@ -88,17 +93,58 @@ const sidebarItems = [
         <div>
           {sidebarItems.map((item, index) => {
             return (
-              <div onClick={() => sidebarHandler(item.text)} key={index} className="flex items-center gap-3 relative hover:bg-gray-100 cursor-pointer rounded-lg p-3 my-3">
+              <div
+                onClick={() => sidebarHandler(item.text)}
+                key={index}
+                className="flex items-center gap-3 relative hover:bg-gray-100 cursor-pointer rounded-lg p-3 my-3"
+              >
                 {item.icon}
                 <span>{item.text}</span>
+                {item.text === "Notifications" &&
+                  likeNotification?.length > 0 && (
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          size="icon"
+                          className="rounded-full h-5 w-5 absolute bottom-6 left-6"
+                        >
+                          {likeNotification.length}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        <div>
+                          {likeNotification.length === 0 ? (
+                            <p>No new notification</p>
+                          ) : (
+                            likeNotification.map((notification) => {
+                              return (
+                                <div key={notification.userId}>
+                                  <Avatar>
+                                    <AvatarImage
+                                      src={
+                                        notification.userDetails?.profilePicture
+                                      }
+                                    />
+                                  </Avatar>
+                                  <p className="text-sm">
+                                    
+                                    <span className="font-bold">{notification.userDetails?.username}liked your post</span>
+                                  </p>
+                                </div>
+                              );
+                            })
+                          )}
+                        </div>
+                      </PopoverContent>
+                    </Popover>
+                  )}
               </div>
             );
           })}
         </div>
       </div>
 
-<CreatePost open={open} setOpen={setOpen} />
-
+      <CreatePost open={open} setOpen={setOpen} />
     </div>
   );
 };
