cff93f1feb9a7ca9c1edaa76f5a6fe1473b3c7ef
Akshita Guru <akshitaguru16@gmail.com>
2025-04-30 22:14:49 +0530

    fixed real time issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,11 +13,16 @@ let persistor = persistStore(store)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-  <App />
-  <Toaster />
-</PersistGate>
-
+      <PersistGate persistor={persistor}>
+        {({ bootstrapped }) =>
+          bootstrapped ? (
+            <>
+              <App />
+              <Toaster />
+            </>
+          ) : null
+        }
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
